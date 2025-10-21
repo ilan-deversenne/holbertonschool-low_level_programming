4c5f1fb46e5c1b2944a37fa7edaf652bf48616f1
ilan-deversenne <ilan.deversenne5@gmail.com>
2025-10-21 15:55:31 +0200

    feat: added 1-last_digit.c
    Genrate random number and print them and if is 0, greater than 5 or less than 6 and not 0

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Generate random number and print them
+ * and if is 0,
+ * greater than 5
+ * less than 6 and not 0
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int l;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	l = n % 10;
+
+	if (l == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l);
+	}
+	else if (l > 5)
+	{
+		printf("Last digit of %d is %d and his greater than 5\n", n, l);
+	}
+	else if (l < 6)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	}
+	else
+	{
+		printf("[ELSE] %d - %d\n", n, l);
+	}
+
+	return (0);
+}
--- variables_if_else_while/README.md
+++ variables_if_else_while/README.md
@@ -1,2 +1,5 @@
 # 0-positive_or_negative.c
 ## Generate random number and print them and if is zero, positive or negative
+
+# 1-last_digit.c
+## Generate random number and print them and if is 0, greater than 5 or less that 6 and not 0
