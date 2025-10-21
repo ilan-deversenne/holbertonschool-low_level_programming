e7d7d90421c349ea122d129958df396a3fb8c0a6
ilan-deversenne <ilan.deversenne5@gmail.com>
2025-10-21 16:46:28 +0200

    feat: Added 2-print_alphabet.c
    Print alphabet but only use putchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print alaphabet only with putchar function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[26] = {
+		97,
+		98,
+		99,
+		100,
+		101,
+		102,
+		103,
+		104,
+		105,
+		106,
+		107,
+		108,
+		109,
+		110,
+		111,
+		112,
+		113,
+		114,
+		115,
+		116,
+		117,
+		118,
+		119,
+		120,
+		121,
+		122
+	};
+
+	int i;
+	for (i = 0; i < 25; i++)
+	{
+		char c = alphabet[i];
+		putchar(c);
+	}
+
+	return (0);
+}
