7462b79f9e1d8d4793cd7c885d97fd3b12ede02d
ilan-deversenne <ilan.deversenne5@gmail.com>
2025-10-24 14:45:34 +0200

    feat: added print_sign function
    Print 0 if number given is 0, + if greater than 0 or -1 if is less than 0

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Print 0 if n is 0, + if is greater than 0 else -
+ * @n: Number to check
+ *
+ * Return: 0 if n is 0
+ * 1 if n is greater than 1
+ * -1 if n is less than 0
+ */
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
