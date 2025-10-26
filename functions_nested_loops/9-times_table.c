4c3ec415f1fc563443478b4576fbe13ab4becdd2
ilan-deversenne <ilan.deversenne5@gmail.com>
2025-10-26 15:46:39 +0100

    feat: added times_table function
    Print 0,9 * 0,9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - Print 0,9 * 0,9
+ *
+ * Return: No return
+ */
+void times_table(void)
+{
+	int x, i, r, first, last;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			r = i * x;
+
+			if (r > 9)
+			{
+				first = r / 10;
+				last = r % 10;
+
+				_putchar('0' + first);
+				_putchar('0' + last);
+			}
+			else
+			{
+				_putchar('0' + r);
+			}
+
+			if (i < 9)
+			{
+				_putchar(',');
+				_putchar(32);
+				_putchar(32);
+			}
+		}
+
+		_putchar(10);
+	}
+}
