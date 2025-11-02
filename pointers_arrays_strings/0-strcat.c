006098641985984e2dd03f2fdc97f2e904e13b2a
ilan-deversenne <ilan.deversenne5@gmail.com>
2025-11-02 22:02:04 +0100

    feat: Added function _strcat
    Concatenates two strings with her pointer

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: Pointer of dest string (First string to concatenates)
+ * @src: Source (Second string to concatenates)
+ * Return: Dest pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	while (*dest != '\0')
+		dest++;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	return (dest);
+}
