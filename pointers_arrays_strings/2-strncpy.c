6e4d99cc68c3967b2d7223af4d4624107b3913c7
ilan-deversenne <ilan.deversenne5@gmail.com>
2025-11-03 15:29:58 +0100

    fix: Added comment for betty

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _strncpy - Copy string
+ * @dest: Destination pointer
+ * @src: Source bytes
+ * @n: Limiter
+ *
+ * Return: (dest) Destination pointer
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
