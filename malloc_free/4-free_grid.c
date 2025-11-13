76923bffd1c0dc410f16888cad1598860491b070
ilan-deversenne <ilan.deversenne5@gmail.com>
2025-11-13 16:43:35 +0100

    feat: Added function free_grid to free grid in memory

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+void free_grid(int **grid, int height)
+{
+	if (grid == NULL)
+	{
+		free(grid);
+	}
+	else if (height == 0)
+	{
+		free(grid[height]);
+		free(grid);
+	}
+	else
+	{
+
+		while (height > 0)
+		{
+			height--;
+			free(grid[height]);
+		}
+
+		free(grid);
+	}
+}
