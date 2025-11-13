be46c4982329c8791671020598bde33d0e1c7f59
ilan-deversenne <ilan.deversenne5@gmail.com>
2025-11-13 22:11:46 +0100

    fix: Srry betty I missed comment :'(

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Dog object
+ * @name: Name of dog
+ * @owner: Owner of dog
+ * @age: Age of dog
+ *
+ * Description: Dog object with name, owner and age
+ */
 struct dog
 {
 	char *name;
