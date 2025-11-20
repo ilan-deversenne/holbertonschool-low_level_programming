a0b9bb68ef12675fb704e8aaab42e34d3c2fa4b2
ilan-deversenne <ilan.deversenne5@gmail.com>
2025-11-21 00:05:07 +0100

    feat: Added main function

---
+++ function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    int num1;
+    int num2;
+    int result;
+    int (*call)(int, int);
+
+    if (argc != 4)
+        return (1);
+
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
+
+    if (argv[2] == NULL)
+        return (1);
+
+    call = get_op_func(argv[2]);
+    if (call == NULL)
+        return (1);
+
+    result = call(num1, num2);
+
+    printf("%d\n", result);
+
+    return (0);
+}
