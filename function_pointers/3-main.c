25f4f7261cdd5704ee930f5d73ecf4e5dca63bb9
ilan-deversenne <ilan.deversenne5@gmail.com>
2025-11-21 00:14:05 +0100

    fix: Some fix for betty

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,29 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[])
+/**
+ * main - Entry point
+ * @argc: Length of args
+ * @argv: Args
+ *
+ * Return: Success = 0
+ * Error = 1
+ */
+int main(int argc, char *argv[])
 {
-    int num1;
-    int num2;
-    int result;
-    int (*call)(int, int);
+	int num1;
+	int num2;
+	int result;
+	int (*call)(int, int);
 
-    if (argc != 4)
-        return (1);
+	if (argc != 4)
+		return (1);
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-    if (argv[2] == NULL)
-        return (1);
+	if (argv[2] == NULL)
+		return (1);
 
-    call = get_op_func(argv[2]);
-    if (call == NULL)
-        return (1);
+	call = get_op_func(argv[2]);
+	if (call == NULL)
+		return (1);
 
-    result = call(num1, num2);
+	result = call(num1, num2);
 
-    printf("%d\n", result);
+	printf("%d\n", result);
 
-    return (0);
+	return (0);
 }
