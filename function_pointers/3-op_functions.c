4ae3f721e981ef095b3ae95e6bbfd6bfb43750ae
ilan-deversenne <ilan.deversenne5@gmail.com>
2025-11-21 00:04:26 +0100

    feat: Added functions for operator: add, sub, mul, div, modulo

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Add two numbers
+ * @a: First number
+ * @b: Last number
+ *
+ * Return: Result of addition
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Sub two numbers
+ * @a: First number
+ * @b: Last number
+ *
+ * Return: Result of sub
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiply two numbers
+ * @a: First number
+ * @b: Last number
+ *
+ * Return: Result of multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divide two numbers
+ * @a: First number
+ * @b: Last number
+ *
+ * Return: Result of division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Modulo of two numbers
+ * @a: First number
+ * @b: Last number
+ *
+ * Return: Result of modulo
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
