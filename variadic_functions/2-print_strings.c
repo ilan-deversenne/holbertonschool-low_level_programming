7366204ee6756452a81d4097eb858375fc75709f
ilan-deversenne <ilan.deversenne5@gmail.com>
2025-11-21 15:31:23 +0100

    feat: Added function print_strings Print strings followed separator from ellipsis

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Print string follwed by separator if not nul
+ * @separator: Separtor or null
+ * @n: Length of ellipsis
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	char *string;
+	unsigned int i;
+	va_list strings;
+
+	va_start(strings, n);
+	for (i = 0; i < n; i++)
+	{
+		string = va_arg(strings, char *);
+		if (string == NULL)
+			printf("(nil)");
+		else
+			printf("%s", string);
+
+		if (i < n - 1)
+			if (separator != NULL)
+				printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(strings);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,12 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif
