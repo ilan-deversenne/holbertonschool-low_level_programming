7c545c6443a025bb86282f2fdee454b0cbdd44fe
ilan-deversenne <ilan.deversenne5@gmail.com>
2025-11-21 09:31:57 +0100

    fix: Added "Error" and exit status code 98 to 100

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -18,17 +18,26 @@ int main(int argc, char *argv[])
 	int (*call)(int, int);
 
 	if (argc != 4)
-		return (1);
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
-	if (argv[2] == NULL)
-		return (1);
+	if ((*argv[2] == '*' || *argv[2] == '/') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
 	call = get_op_func(argv[2]);
 	if (call == NULL)
-		return (1);
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
 	result = call(num1, num2);
 
