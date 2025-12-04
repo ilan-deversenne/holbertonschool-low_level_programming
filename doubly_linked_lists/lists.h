e30f0958c46a952d5c6167e985608dc035504149
ilan-deversenne <ilan.deversenne5@gmail.com>
2025-12-04 14:07:44 +0100

    fix: Betty style

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,7 +1,7 @@
-#include <stdlib.h>
-
 #ifndef LISTS_H
 #define LISTS_H
+
+#include <stdlib.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -10,13 +10,12 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
