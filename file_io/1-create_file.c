096aa27d8279f452e2a169103885aedff1727de8
ilan-deversenne <ilan.deversenne5@gmail.com>
2025-12-05 14:38:23 +0100

    feat: Added create_file function
    Create file with content or not

---
+++ file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * create_file - Create a file with content or not
+ * @filename: Name of the file
+ * @text_content: Content of file or null
+ *
+ * Return: (Success) 1
+ * (Error) -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len;
+	ssize_t bytesWritten;
+
+	len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
+	while (*(text_content + len) != '\0')
+		len++;
+
+	bytesWritten = write(fd, text_content, len);
+	if (bytesWritten == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
