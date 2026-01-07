21063857d77f4ff35826db01bacf119fbf8b4751
Ilan Deversenne <ilan.deversenne@holbertonstudents.com>
2026-01-07 18:27:03 +0100

    fix: Added return NULL and added \0 at end of string

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,8 +11,8 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, len1, len2;
 	char *result = NULL;
+	int i, y, len1, len2;
 
 	len1 = 0;
 	len2 = 0;
@@ -23,13 +23,17 @@ char *str_concat(char *s1, char *s2)
 	while (s2[len2] != '\0')
 		len2++;
 
-	result = (char *) malloc((len1 + len2) * sizeof(char));
+	result = (char *) malloc((len1 + len2 + 1) * sizeof(char));
+	if (result == NULL)
+		return (NULL);
 
 	for (i = 0; i < len1; i++)
 		result[i] = s1[i];
 
-	for (i = 0; i < len2; i++)
-		result[len1 + i] = s2[i];
+	for (y = 0; y < len2; y++)
+		result[len1 + y] = s2[y];
+
+	result[i + y] = "\0";
 
 	return (result);
 }
